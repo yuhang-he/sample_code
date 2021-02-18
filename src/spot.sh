03f248c7484e09fde3310bd95b6dae2fdf89ee27
Yu Hang He <yuhanghe.pro@gmail.com>
2021-02-18 15:39:43 -0800

    Delete spot.sh

--- src/spot.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin
-python spot_gui.py --user user --password yebliwoumo --app-token C:\Users\yuhhe\.bosdyn\tesla.token 192.168.80.3
-
-python spot_gui.py --user user --password rysheypuka --app-token C:\Users\yuhhe\.bosdyn\tesla.token 192.168.80.3
